37ec97f249ef926f50df8197c1c1026d7cce4939
musikele <michele.nasti@gmail.com>
2017-02-13 15:49:00 +0100

    create soft link to wp-content in /vagrant/

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,16 @@ sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password passwor
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
 sudo apt-get -y install mysql-server
 
+sudo adduser ubuntu www-data
+sudo cp /vagrant/envvars /etc/apache2/envvars
+
 # script che inizializza il db 
 cat /vagrant/wordpress.sql | sudo mysql --defaults-extra-file=/etc/mysql/debian.cnf
 
-#if ! [ -L /var/www ]; then
-#	rm -rf /var/www
-#	ln -fs /vagrant /var/www
-#fi
+if ! [ -L /var/www ]; then
+	rm -rf /var/www/html/wp-content
+	sudo ln -fs /vagrant/wp-content /var/www/html/wp-content
+fi
 
 ## scarico wordpress e lo unzippo 
 cd /tmp
@@ -27,18 +30,17 @@ unzip -oq latest.zip -d /var/www/html/
 # creo cartella wp-content/uploads
 mkdir -p /var/www/html/wordpress/wp-content/uploads
 
-# assegno i permessi 
-chown -R www-data:www-data /var/www/
-chmod -R 755 /var/www/html/wordpress
-
 # copio mio file di configurazione in wordpress
 /bin/cp -fR /vagrant/wp-config.php /var/www/html/wordpress
 
 # sposto wordpress nella root del webserver 
 cd /var/www/html/
 cp -r wordpress/* ./
-rm index.html
 rm -rf /var/www/html/wordpress/
+
+# assegno i permessi 
+sudo chown -R www-data:www-data /var/www/
+sudo chmod -R 755 /var/www/html/
 
 #restart di apache
 sudo service apache2 restart
